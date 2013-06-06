d57cc526b9afb962aef942e09b320ce9e0e5e0df
miodas <wielkimiodas@gmail.com>
2013-06-06 14:11:42 +0200

    Nowa wersja szablonu

--- RaceGamePhone/Properties/AssemblyInfo.cs
+++ RaceGamePhone/Properties/AssemblyInfo.cs
@@ -1,28 +1,27 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("RaceGamePhone")]
-[assembly: AssemblyProduct("RaceGamePhone")]
 [assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RaceGamePhone")]
 [assembly: AssemblyCopyright("Copyright ©  2013")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type. Only Windows
-// assemblies support COM.
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-// On Windows, the following GUID is for the ID of the typelib if this
-// project is exposed to COM. On other platforms, it unique identifies the
-// title storage container when deploying this assembly to the device.
-[assembly: Guid("ab201434-3037-4f31-b7ab-42d5c613d22e")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f020f7fc-4c01-4ffc-832a-85bfa56d4776")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -31,4 +30,8 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
 [assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: NeutralResourcesLanguageAttribute("en-US")]
